c738e8f108fe5603e049b42f7a8987335b3e8a94
ritikranjan567 <ranjanritik219@gmail.com>
2020-03-19 20:01:37 +0530

    Active storage added for storing images

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :assets, dependent: :destroy
   has_one :wishlist, dependent: :destroy
   has_many :bookings, dependent: :destroy
+  has_one_attached :profile_picture
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :lockable,
