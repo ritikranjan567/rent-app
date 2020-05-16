76aa81863273c31e3d730cec75ac2097614a950a
ritikranjan567 <ranjanritik219@gmail.com>
2020-05-16 10:04:06 +0530

    Seen notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = Notification.where(user_id: current_user.id).news
+    @notifications.update_all(viewed: true)
   end
 
   private
